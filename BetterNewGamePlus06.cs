c44349593e4c477280e6a97758317ca3adcce80b
Matthiew Purple <matthiewpurple@hotmail.com>
2023-03-17 12:23:50 +0100

    added description

--- BetterNewGamePlus06.cs
+++ BetterNewGamePlus06.cs
@@ -4,7 +4,7 @@ using Il2Cpp;
 using better_new_game_plus_06;
 using Il2Cppnewdata_H;
 
-[assembly: MelonInfo(typeof(BetterNewGamePlus06), "Better New Game Plus (0.6 ver.)", "1.0.0", "Matthiew Purple & xenopiece")]
+[assembly: MelonInfo(typeof(BetterNewGamePlus06), "Better New Game Plus [demons only] (0.6 ver.)", "1.0.0", "Matthiew Purple & xenopiece")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace better_new_game_plus_06;
